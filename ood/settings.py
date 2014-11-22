a5859426a3fcff4132af63dbdcf12ead68ea6294
lpe234 <lpe234@qq.com>
2014-11-22 21:37:24 +0800

    fix settings

--- ood/settings.py
+++ ood/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3',      # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'sho.sqlite3',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         # 'USER': 'root',
         # 'PASSWORD': '12345',
-        # 'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        # 'HOST': '',             # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         # 'PORT': '3306',                      # Set to empty string for default.
     }
 }
